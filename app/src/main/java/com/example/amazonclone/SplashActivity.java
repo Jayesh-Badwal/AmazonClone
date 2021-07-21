e6a86cd833c1772342e693b1a23cc665550b05c1
Jayesh-Badwal <badwaljayesh15@gmail.com>
2021-07-21 22:27:36 +0530

    Splash screen and search bar

---
+++ app/src/main/java/com/example/amazonclone/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.amazonclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        splashScreen();
+    }
+
+    private void splashScreen() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }
+            }
+        }).start();
+    }
+}
